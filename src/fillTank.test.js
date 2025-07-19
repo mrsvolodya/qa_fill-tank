0fd44ee1a59ecaba61889b141416e01b94f26350
mrsvolodya <volodyamurskyi@gmail.com>
2025-07-19 15:13:19 +0300

    Finish fill tank task

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,92 @@
-'use strict';
+"use strict";
 
-describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+describe("fillTank", () => {
+  const { fillTank } = require("./fillTank");
 
-  it('should ', () => {
+  it(`If the 'amount' is not given, then full tank is ordered`, () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
 
+    fillTank(customer, 5);
+
+    expect(customer.vehicle.fuelRemains).toBe(40);
   });
 
-  // write tests here
+  it(`If the 'amount' is greater than the tank can accommodate,
+     pour only what will fit.`, () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 5, 3000);
+
+    expect(customer.vehicle.fuelRemains).toBe(40);
+  });
+
+  it(`should always fill tank in only what the client can pay.`, () => {
+    const customer = {
+      money: 4,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 2, 2);
+
+    expect(customer.vehicle.fuelRemains).toBe(10);
+  });
+
+  it(`should round the poured amount 
+    by discarding number to the tenth part`, () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 5.44, 5.44);
+
+    expect(customer.vehicle.fuelRemains).toBe(13.4);
+  });
+
+  it(`If the poured amount is less than 2 liters, 
+    do not pour at all.`, () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 5.44, 1);
+
+    expect(customer.vehicle.fuelRemains).toBe(8);
+  });
+
+  it(`Round the price of the purchased fuel the to the nearest hundredth part.`, () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 5.4455, 2);
+
+    expect(customer.money).toBe(2989.11);
+  });
 });
